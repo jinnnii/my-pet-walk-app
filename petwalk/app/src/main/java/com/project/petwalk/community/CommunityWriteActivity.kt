c39d3dbf0b7f5ab7701216fde86f73a05a162dd7
RharzAhn <dksdudfbs@gmail.com>
2022-05-16 15:18:27 +0900

    Error 댓글 버튼구현필요

--- petwalk/app/src/main/java/com/project/petwalk/community/CommunityWriteActivity.kt
+++ petwalk/app/src/main/java/com/project/petwalk/community/CommunityWriteActivity.kt
@@ -13,8 +13,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ServerValue
+import com.project.petwalk.Comment
 import com.project.petwalk.Post
 import com.project.petwalk.R
+import com.project.petwalk.frag.FragCommunity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_community_write.*
 import kotlinx.android.synthetic.main.card_background.view.*
@@ -22,6 +24,13 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class CommunityWriteActivity : AppCompatActivity() {
+
+    // 글쓰기모드를 저장하는 변수
+    var mode = "post"
+
+    // 댓글쓰기인 경우 글의 ID
+    var postId = ""
+
     //    배경 리스트 데이터
 //    res/drawable 디렉토리에 있는 배경 이미지를 uri 주소로 사용한다.
 //    uri 주소로 사용하면 추후 웹에 있는 이미지 URL 도 바로 사용이 가능하다.
@@ -46,6 +55,12 @@ class CommunityWriteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community_write)
 
+        //전달받은 intent 에서 댓글 모드인지 확인
+        intent.getStringExtra("mode")?.let{
+            mode = intent.getStringExtra("mode")!!
+            postId = intent.getStringExtra("postId")!!
+        }
+
         // actionbar 의 타이틀을 "글쓰기"로 변경
         supportActionBar?.title = "글쓰기"
 
@@ -66,30 +81,51 @@ class CommunityWriteActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "메세지를 입력하세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            // Post 객체 생성
-            val post = Post()
-            // Firebase 의 Posts 참조에서 객체를 저장하기 위한 새로운 키를 생성하고 참조를 newRef 에 저장
-            val newRef = FirebaseDatabase.getInstance().getReference("Posts").push()
-            // 글이 쓰여진 시간은 Firebase 서버의 시간으로 설정
+            if(mode == "post") {
+
+                // Post 객체 생성
+                val post = Post()
+                // Firebase 의 Posts 참조에서 객체를 저장하기 위한 새로운 키를 생성하고 참조를 newRef 에 저장
+                val newRef = FirebaseDatabase.getInstance().getReference("Posts").push()
+                // 글이 쓰여진 시간은 Firebase 서버의 시간으로 설정
 //            val time = System.currentTimeMillis()
 //            val dateFormat = SimpleDateFormat("yyyy-MM-dd hh:mm")
 //            val curTime = dateFormat.format(Date(time)).toString()
 
-            post.writeTime = ServerValue.TIMESTAMP
-            //post.writeTime = SimpleDateFormat("yyyy-MM-dd hh:mm")
-            // 배경 Uri 주소를 현재 선택된 배경의 주소로 할당
-            post.bgUri = bgList[currentBgPosition]
-            // 메세지는 input EditText 의 텍스트 내용을 할당
-            post.message = input.text.toString()
-            // 글쓴 사람의 ID 는 디바이스의 아이디로 할당
-            post.writerId = getMyId()
-            // 글의 ID 는 새로 생성된 파이어베이스 참조의 키로 할당
-            post.postId = newRef.key.toString()
-            // Post 객체를 새로 생성한 참조에 저장
-            newRef.setValue(post)
-            // 저장성공 토스트 알림을 보여주고 Activity 종료
-            Toast.makeText(applicationContext, "공유되었습니다", Toast.LENGTH_SHORT).show()
-            finish()
+                post.writeTime = ServerValue.TIMESTAMP
+                //post.writeTime = SimpleDateFormat("yyyy-MM-dd hh:mm")
+                // 배경 Uri 주소를 현재 선택된 배경의 주소로 할당
+                post.bgUri = bgList[currentBgPosition]
+                // 메세지는 input EditText 의 텍스트 내용을 할당
+                post.message = input.text.toString()
+                // 글쓴 사람의 ID 는 디바이스의 아이디로 할당
+                post.writerId = getMyId()
+                // 글의 ID 는 새로 생성된 파이어베이스 참조의 키로 할당
+                post.postId = newRef.key.toString()
+                // Post 객체를 새로 생성한 참조에 저장
+                newRef.setValue(post)
+                // 저장성공 토스트 알림을 보여주고 Activity 종료
+                Toast.makeText(applicationContext, "공유되었습니다", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                val comment = Comment()
+                // Firebase 의 Posts 참조에서 객체를 저장하기 위한 새로운 키를 생성하고 참조를 newRef 에 저장
+                val newRef = FirebaseDatabase.getInstance().getReference("Comments/$postId").push()
+                comment.writeTime = ServerValue.TIMESTAMP
+                comment.bgUri = bgList[currentBgPosition]
+                // 메세지는 input EditText 의 텍스트 내용을 할당
+                comment.message = input.text.toString()
+                // 글쓴 사람의 ID는 디바이스의 아이디로 할당
+                comment.writerId = getMyId()
+                // 글의 ID 는 새로 생성된 파이어베이스 참조의 키로 할당
+                comment.commentId = newRef.key.toString()
+                // 댓글이 속한 글의 ID
+                comment.postId = postId
+                newRef.setValue(comment)
+                // 저장성공 토스트 알림을 보여주고 Activity 종료
+                Toast.makeText(applicationContext, "공유되었습니다.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
         }
     }
 
