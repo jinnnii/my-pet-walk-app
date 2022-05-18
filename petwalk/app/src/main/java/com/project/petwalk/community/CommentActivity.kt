af19f5c2513e6476b5491a5a10cfb05a431e29dc
RharzAhn <dksdudfbs@gmail.com>
2022-05-18 16:37:28 +0900

    Error : 댓글기능 제작중, 미완성으로 인한 오류발생파일

--- petwalk/app/src/main/java/com/project/petwalk/community/CommentActivity.kt
+++ petwalk/app/src/main/java/com/project/petwalk/community/CommentActivity.kt
@@ -2,11 +2,25 @@ package com.project.petwalk.community
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.project.petwalk.Comment
 import com.project.petwalk.R
+import com.project.petwalk.databinding.ActivityCommentBinding
 
 class CommentActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityCommentBinding
+    val comment = Comment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comment)
+        binding = ActivityCommentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fireDatabase = FirebaseDatabase.getInstance() //f -> 파이어베이스데이터베이스의 자료를 담음
+        var database = Firebase.database.reference // 데이터베이스레퍼런스를 위한 인스턴트 생성
+
+
     }
 }
